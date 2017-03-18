a0ba73fd6d407f980be14771c1511b27b660cb0e
Liudmila Molkova <lmolkova@microsoft.com>
2017-03-17 18:58:11 -0700

    up

---
+++ Src/Web/Web.Shared.Net.Tests/Helpers/TEstOperationHolder.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Microsoft.ApplicationInsights.Web.Helpers
+{
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.Extensibility;
+
+    public class TestOperationHolder : IOperationHolder<RequestTelemetry>
+    {
+        public TestOperationHolder(RequestTelemetry telemetry)
+        {
+            Telemetry = telemetry;
+        }
+        public TestOperationHolder()
+        {
+            Telemetry = new RequestTelemetry();
+        }
+        public void Dispose()
+        {
+        }
+
+        public RequestTelemetry Telemetry { get; private set; }
+    }
+}
