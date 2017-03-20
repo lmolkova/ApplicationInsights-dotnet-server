be40b67d7e64d4611ea54cf1188f24735c379855
Liudmila Molkova <lmolkova@microsoft.com>
2017-03-20 11:31:14 -0700

    up

---
+++ Src/TestFramework/Shared/TelemetryContextExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.ApplicationInsights.TestFramework
+{
+    using System.Collections.Concurrent;
+    using System.Reflection;
+    using Microsoft.ApplicationInsights.DataContracts;
+
+    internal static class TelemetryContextExtensions
+    {
+        internal static ConcurrentDictionary<string,string> GetCorrelationContext(this TelemetryContext context)
+        {
+            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+            var field = typeof(TelemetryContext).GetField("correlationContext", bindFlags);
+            return (ConcurrentDictionary<string, string>)field.GetValue(context);
+        }
+    }
+}
